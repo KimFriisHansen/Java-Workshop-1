6aa2f88ab2173323d7dd59f15d334093fe80f7d2
Kim Friis Hansen <kim@kimfriishansen.dk>
2019-02-24 23:24:51 +0100

    Add files via upload

---
+++ Java8-Workshop-part-2/DatesAndTimes/Main.java
@@ -0,0 +1,22 @@
+package org.example.java8;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Indsæt kode currentTime
+		System.out.println("the current time: "+currentTime);
+		
+		// Indsæt kode specificTime;
+		System.out.println("the specific time: "+specificTime);
+		
+		// Indsæt kode  currentDateTime
+		System.out.println("the current date and time: "+currentDateTime);
+		
+		
+	}
+
+}
